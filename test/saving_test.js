c4785057168b37ccbaffc7d6febc1323daad11b0
Merav <merav.mi@gmail.com>
2019-11-16 11:59:53 +0200

    added a before method to assert a connection to db is established *prior* to testing

--- test/saving_test.js
+++ test/saving_test.js
@@ -1,25 +1,39 @@
-const mocha = require('mocha');
 const assert = require('assert');
 const DogModel = require('../models/dog');
+const mongoose = require('mongoose');
+mongoose.set('useUnifiedTopology', true);
 
-describe('saving records', function() {
-    
+
+describe('saving records', function () {
+
+    // making sure a connection to the db is established *prior* to testing
+    before((done) => {
+        mongoose.connect('mongodb://localhost/testing123', {
+                useNewUrlParser: true
+            })
+            .then(console.log('connection has been made'))
+            .catch('error', function (error) {
+                console.log('Connection error:', error);
+            });
+        done();
+    });
+
     // create tests
-    it ('saves a new dog in the db', function(done) {
+    it('saves a new dog in the db', function (done) {
         let dog = new DogModel({
             name: 'Celina',
             breed: 'mixed',
             chip: 123456789012345
         });
 
-        dog.save().then(function(){
+        dog.save().then(function () {
             assert(dog.isNew === false);
         });
 
         done();
 
-    }); 
-        
+    });
+
     // next test
 
 });
