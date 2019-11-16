daf3c6c616e64086e05c2e7e7e0917965abc22ff
Merav <merav.mi@gmail.com>
2019-11-16 15:57:53 +0200

    added test to finding record by id

--- test/finding_test.js
+++ test/finding_test.js
@@ -2,8 +2,11 @@ const assert = require('assert');
 const DogModel = require('../models/dog');
 
 describe('Finding records', function () {
+
+    let newDog;
+
     beforeEach(function (done) {
-        let newDog = new DogModel({
+        newDog = new DogModel({
             name: 'Celina',
             breed: 'mixed',
             chip: 123456789012345
@@ -18,11 +21,27 @@ describe('Finding records', function () {
 
     it('Finds one record in the db', function (done) {
         // find & findOne are asynchronous, therefore require resolving a response with .then
-        DogModel.findOne({
-            breed: 'mixed'
-        }).then(function (res) {
-            assert(res.breed === 'mixed');
-        });
+
+        DogModel.findOne({breed: 'mixed'})
+            .then(function (res) {
+                assert(res.breed === 'mixed');
+            })
+            .catch(err => console.error(err));
+
         done();
     });
+
+
+    it('Finds one record in the db by id', function (done) {
+        // find & findOne are asynchronous, therefore require resolving a response with .then
+
+        DogModel.findOne({_id: newDog._id})
+            .then(function (res) {
+                assert(res._id.toString() === newDog._id.toString());
+            })
+            .catch(err => console.error(err));
+
+        done();
+    });
+
 });
